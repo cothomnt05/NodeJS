7d78db4163ff7c08e32ff5f271ebdaa833e37144
cothomnt05 <cothomnt05@gmail.com>
2022-06-07 11:15:00 +0700

    update 2

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
+//Client nhap todoList sau đó sẽ lưu và server bằng REST API , mô hình MVC, localhost:3000/api/todos/id
+//sự khác nhau REST(protocol)/RPC
+
 const path = require("path");
 const express = require("express");
 // const morgan = require("morgan");
@@ -7,7 +10,9 @@ const port = 8000;
 
 const route = require("./routes/index");
 
+//dẫn trực tiếp tới các file tĩnh như hình ảnh CSS file hoặc JS file cú pháp express.static
 app.use(express.static(path.join(__dirname, "public")));
+//json và urlcoded là 1 phần của bodyparser
 app.use(
   express.urlencoded({
     extended: true,
